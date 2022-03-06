b2fabc6bfc32804686408c51e0a4eed6802eba20
PTMAndrey <dorinandries766@yahoo.com>
2022-03-06 13:37:33 +0200

    added main component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Helmet } from "react-helmet";
+
 
 ReactDOM.render(
   <React.StrictMode>
+  <Helmet>
+    <title>Tech Challenge</title>
+  </Helmet>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
