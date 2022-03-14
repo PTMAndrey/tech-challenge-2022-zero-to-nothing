4aa7724fd4aeb511a653e0d16b22cfb4257f42ca
Andrieș Dorin-Andrei <30751450+PTMAndrey@users.noreply.github.com>
2022-03-14 18:59:33 +0200

    Create GlobalStyle.js

---
+++ src/Assets/GlobalStyle/GlobalStyle.js
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    scrollbar-width: thin;
+    scrollbar-color: rgb(185, 185, 185) transparent;
+  }
+  
+  body {
+    font-family: 'Roboto', sans-serif;
+    overflow-x: hidden;
+  }
+  *::-webkit-scrollbar {
+    width: 5px;
+  }
+  *::-webkit-scrollbar-track {
+    background: transparent;
+  }
+  *::-webkit-scrollbar-thumb {
+    background: rgb(185, 185, 185);
+    border-radius: 20px;
+    border: transparent;
+  }
+`;
